32d70def19c616b967118c871f149fcbd18c76e3
ghadiben0 <ghadiben099@gmail.com>
2025-07-01 10:18:44 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,8 +19,6 @@ extensions = [
     'sphinx.ext.intersphinx',
 ]
 
-extensions += ['hoverxref.extension']
-
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
